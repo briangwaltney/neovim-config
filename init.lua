a02a963c6e42239a7a7ee97a3d2002c7ef1f8d4c
Brian Gwaltney <brian.gwaltney@gmail.com>
2022-09-09 22:33:38 +0300

    trying to get git to work

--- init.lua
+++ init.lua
@@ -1,8 +1,14 @@
 require "user.options"
-require "user.keybinds"
+require "user.keymap"
 require "user.plugins"
 require "user.colorscheme"
 require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopair"
+require "user.comment"
+require "user.gitsigns"
 
 function _G.ReloadConfig()
   for name,_ in pairs(package.loaded) do
